41ab33f5272208efa26d105a3a26f17438aaba90
abbyrhart <94138365+abbyrhart@users.noreply.github.com>
2022-11-03 14:49:18 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -23,13 +23,15 @@ document.querySelector("#pause").addEventListener("click", function() {
 });
  
 document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow down video");
 	video.playbackRate *= .9;
-	console.log("New playback rate is " + video.playbackRate);
+	console.log("Speed is " + video.playbackRate);
 });
 
 document.querySelector("#faster").addEventListener("click", function(){
+	console.log("Speed up video");
 	video.playbackRate *= 1.1;
-	console.log("New playback rate is " + video.playbackRate);
+	console.log("Speed is " + video.playbackRate);
 });
 
 document.querySelector("#skip").addEventListener("click", function(){
@@ -45,16 +47,22 @@ document.querySelector("#mute").addEventListener("click", function(){
 	if (video.muted) {
 		video.muted = false;
 		document.querySelector("#mute").innerHTML = "Mute";
+		console.log("Unmute")
+
 	} else {
 		video.muted = true;
 		document.querySelector("#mute").innerHTML = "Unmute";
+		console.log("Mute")
+
 	}
 });
 
-document.querySelector("#slider").addEventListener("oninput", function(){
-	var vol = document.querySelector("#slider").value; 
-	document.querySelector("#volume").innerHTML =  vol + "%";
-	video.volume = vol / 100;
+document.querySelector("#slider").addEventListener("click", function(){
+	console.log("Volume Slider")
+	console.log("Current volume is " + video.volume)
+	video = document.querySelector("#player1"); 
+	video.volume = this.value / 100;
+	document.querySelector("#volume").innerHTML =  video.volume * 100 + "%";
 });
 
 document.querySelector("#vintage").addEventListener("click", function(){
